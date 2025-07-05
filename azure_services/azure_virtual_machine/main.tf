f124e40c2d8cddac74005a21051efca17829b1e3
devendraSinghtomar24 <99275680+devendraSinghtomar24@users.noreply.github.com>
2025-07-05 16:55:09 +0530

    add vm code (#3)
    * add vm code
    
    * add new vm code and delete

---
+++ azure_services/azure_virtual_machine/main.tf
@@ -0,0 +1,39 @@
+resource "azurerm_network_interface" "vm_dev_eus_01_nic" {
+  name                = "nic-dev-eus-01"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm_dev_eus_01" {
+  name                = "vm-dev-eus-01"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.vm_dev_eus_01_nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
